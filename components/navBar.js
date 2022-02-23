bb0bece47385f637717582bbc2bea2b0b570ed89
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-02-23 22:13:23 +0100

    navigation

--- components/navBar.js
+++ components/navBar.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from "next/router";
 
 function navBar() {
+	const router = useRouter();
+	// console.log(Link);
+
 	return (
 		<>
-			<nav>
-				<Link href="#">
-					<a data-text="home">
-						home
-					</a>
-				</Link>
-				<Link href="#">
-					<a data-text="about">
-						about
-					</a>
-				</Link>
-				<Link href="#">
-					<a data-text="projects">
-						projects
-					</a>
-				</Link>
-			</nav>
+			<nav className='navigation'>
+				<ul>
+					<li className={(router.asPath == "/#home") ? "active" : ""} data-text="home">
+						<Link href="#home">home</Link>
+					</li>
+					<li className={router.asPath == "/#about" ? "active" : ""} data-text="about">
+						<Link href="#about">about</Link>
+					</li>
+					<li className={router.asPath == "/#work" ? "active" : ""} data-text="work">
+						<Link href="#work">work</Link>
+					</li>
+					<li className={router.asPath == "/#hire-me" ? "active" : ""} data-text="hire me">
+						<Link href="#hire-me">hire me</Link>
+					</li>
+				</ul >
+
+			</nav >
 		</>
 	);
 }

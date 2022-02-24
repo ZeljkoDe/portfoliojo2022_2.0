6753b07c31e38cf52796f5ba8ebf9bd4b7b69128
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-02-24 06:19:47 +0100

    navigation

--- components/navBar.js
+++ components/navBar.js
@@ -4,26 +4,25 @@ import { useRouter } from "next/router";
 
 function navBar() {
 	const router = useRouter();
-	// console.log(Link);
+	const navigation = [
+		{ item: 'home', path: '/#home' },
+		{ item: 'about', path: '/#about' },
+		{ item: 'work', path: '/#work' },
+		{ item: 'hire me', path: '/#hire-me' },
+	];
 
 	return (
 		<>
 			<nav className='navigation'>
 				<ul>
-					<li className={(router.asPath == "/#home") ? "active" : ""} data-text="home">
-						<Link href="#home">home</Link>
-					</li>
-					<li className={router.asPath == "/#about" ? "active" : ""} data-text="about">
-						<Link href="#about">about</Link>
-					</li>
-					<li className={router.asPath == "/#work" ? "active" : ""} data-text="work">
-						<Link href="#work">work</Link>
-					</li>
-					<li className={router.asPath == "/#hire-me" ? "active" : ""} data-text="hire me">
-						<Link href="#hire-me">hire me</Link>
-					</li>
+					{navigation.map((nav, i) => {
+						return (
+							<li key={i} className={router.asPath === nav.path ? 'active' : ''} data-text={nav.item}>
+								<Link href={nav.path}><a>{nav.item}</a></Link>
+							</li>
+						);
+					})}
 				</ul >
-
 			</nav >
 		</>
 	);
